7be2dd47d17286c75f9a603d660b81d84e5738b8
Lugaya <lugayamuktias@gmail.com>
2024-03-27 12:22:59 +0700

    hosting web

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def mm_to_inches(mm):
+    return mm / 25.4
+
+def calculate_clearance(x, t, u):
+    p = 2.277 - (x - t + u)
+    return p
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        x = float(request.form['x'])
+        t = float(request.form['t'])
+        u = float(request.form['u'])
+        unit = request.form['unit']
+
+        if unit == 'mm':
+            x = mm_to_inches(x)
+            t = mm_to_inches(t)
+            u = mm_to_inches(u)
+
+        p = calculate_clearance(x, t, u)
+        return render_template('calculator.html', result=p)
+    return render_template('calculator.html', result=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
